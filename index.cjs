24b874db41d1032016125aab3beeb46edfc19d3d
Guthib-of-Dan <diril656@gmail.com>
2025-08-29 15:25:01 +0000

    Black text is more visible on top of colourful background

--- index.cjs
+++ index.cjs
@@ -2,16 +2,16 @@ var { styleText } = require("node:util");
 var console = require("node:console");
 var logger = {
   error(main, ...data) {
-    console.error(styleText(["bgRed"], main), ...data);
+    console.error(styleText(["bgRed", "black"], main), ...data);
   },
   log(main, ...data) {
     console.log(styleText(["gray"], main), ...data);
   },
   info(main, ...data) {
-    console.info(styleText(["bgGreen"], main), ...data);
+    console.info(styleText(["bgGreen", "black"], main), ...data);
   },
   warn(main, ...data) {
-    console.warn(styleText(["bgYellow"], main), ...data);
+    console.warn(styleText(["bgYellow", "black"], main), ...data);
   },
   group(label) {
     console.group(styleText(["cyan"], "--- logs " + label + "---"));
